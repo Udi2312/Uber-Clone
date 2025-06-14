8e24b954962ce0897e9bff86c265d33724a61087
Udit Bansal <Udit.ban2312@gmail.com>
2025-06-14 20:15:20 +0530

    Commit

--- server/app.js
+++ server/app.js
@@ -4,10 +4,16 @@ const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors');
 app.use(cors());
+const connectToDb = require('./db/db');
+connectToDb();
+const userRoutes = require('./routes/user.routes');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+app.use('/users', userRoutes);
 module.exports = app;
